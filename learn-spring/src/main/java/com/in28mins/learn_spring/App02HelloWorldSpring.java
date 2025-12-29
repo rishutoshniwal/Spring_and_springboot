b20a3b126d9fb013cce9a109b16ff156c9bab775
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2025-12-29 20:38:56 +0530

    add some more beans

--- learn-spring/src/main/java/com/in28mins/learn_spring/App02HelloWorldSpring.java
+++ learn-spring/src/main/java/com/in28mins/learn_spring/App02HelloWorldSpring.java
@@ -8,6 +8,9 @@ public class App02HelloWorldSpring {
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
 
 
 	}
--- learn-spring/src/main/java/com/in28mins/learn_spring/HelloWorldConfiguration.java
+++ learn-spring/src/main/java/com/in28mins/learn_spring/HelloWorldConfiguration.java
@@ -3,12 +3,34 @@ package com.in28mins.learn_spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {};
+
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
 	@Bean
 	public String name() {
 		return "Rishu";
+	}
+	
+	@Bean
+	public int age() {
+		return 26;
+		
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Sonam", 28);
+		
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Nadrai Gate", "Kasganj");
+		
 	}
 
 }
