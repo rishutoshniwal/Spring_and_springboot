e48672ee57814670eece49fab40d0c9a1463e807
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-01-10 17:03:43 +0530

    using Qualifier to avoid - No unique bean found exception

--- learn-spring-2/src/main/java/com/in28mins/learn_spring/game/GameRunner.java
+++ learn-spring-2/src/main/java/com/in28mins/learn_spring/game/GameRunner.java
@@ -1,12 +1,13 @@
 package com.in28mins.learn_spring.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
 	GamingConsole game;
 
-	public GameRunner(GamingConsole game) {
+	public GameRunner(@Qualifier("SuperContraQualifier")GamingConsole game) {
 		this.game = game;
 	}
 
