fa9d89bdd97a8b1550094b9613f1fd0bbb83e166
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-01-10 20:29:39 +0530

    Field Injection using decorator: Autowired

--- learn-spring-2/src/main/java/com/in28mins/learn_spring/game/example/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-2/src/main/java/com/in28mins/learn_spring/game/example/a1/DependencyInjectionLauncherApplication.java
@@ -1,12 +1,35 @@
 package com.in28mins.learn_spring.game.example.a1;
 
-import java.util.Arrays;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-import com.in28mins.learn_spring.game.GameRunner;
+@Component
+class YourBusinessClass {
+	
+	@Autowired
+	Dependency1 dep1;
+	
+    @Autowired
+	Dependency2 dep2;
+	
+	public String toString() {
+		return "Field Injection: Dependency1 --> " + dep1 +" \n Dependency2 --> " + dep2;
+	}
+	
+}
+
+@Component
+class Dependency1 {
+	
+}
+
+@Component
+class Dependency2 {
+	
+}
 
 @Configuration
 @ComponentScan
@@ -15,7 +38,9 @@ public class DependencyInjectionLauncherApplication {
 	public static void main(String[] args) {
 		var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class);
 		
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+//		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		
+		System.out.println(context.getBean(YourBusinessClass.class));
 	
 	}
 
