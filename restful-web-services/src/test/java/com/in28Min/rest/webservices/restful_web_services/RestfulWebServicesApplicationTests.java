7e1f4e0fd4a1d6f04ecfe84c3c3f0889b7032199
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-02-08 13:20:23 +0530

    Setup from spring.io

---
+++ restful-web-services/src/test/java/com/in28Min/rest/webservices/restful_web_services/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28Min.rest.webservices.restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
