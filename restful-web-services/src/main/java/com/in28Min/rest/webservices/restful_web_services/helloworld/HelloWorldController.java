7e530919c0e6976d86e6ff1213cc09a46ff4c54b
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-02-08 13:22:24 +0530

    lecture: 121 end

--- restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,15 +1,14 @@
 package com.in28Min.rest.webservices.restful_web_services.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(method = RequestMethod.GET, path = "hello-world")
+	@GetMapping(path = "hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World1";
 	}
 
 }
