2931fcfa57d338f95bbed8f71da2ff65a3ac362b
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-02-08 13:26:05 +0530

    lecture: 122 end

---
+++ restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/helloworld/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.in28Min.rest.webservices.restful_web_services.helloworld;
+
+public class HelloWorldBean {
+	
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+
+}
