09635add1a0df04768bedef81b157c9b37d2b6ae
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-02-15 16:12:20 +0530

    lecture 147: Spring Boot inbuilt versioning: All 4 types : URI, Request Params, Header and accept header

---
+++ restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/versioning/SpringBootVersioningPersonController.java
@@ -0,0 +1,32 @@
+package com.in28Min.rest.webservices.restful_web_services.versioning;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootVersioningPersonController {
+
+//	@GetMapping(value="/{version}/personsbi", version="1.0.0")
+//	public PersonV1 getFirstVersionOfPerson() {
+//		return new PersonV1("Bob2 Charlie");
+//	}
+//
+//	@GetMapping(value="/{version}/personsbi", version="2.0.0")
+//	public PersonV2 getSecondVersionOfPerson() {
+//		return new PersonV2(new Name("Bob", "Charlie2"));
+//	}
+//
+//	@GetMapping(path = "/personsbi", version="1.0.0")
+//	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+//		return new PersonV1("Alice3 Singh2");
+//	}
+//
+//	@GetMapping(path = "/personsbi", version="2.0.0")
+//	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+//		return new PersonV2(new Name("Bob", "Kumar3"));
+//	}
+
+}
