5e9322331604e24baad3e4fd42c38b6015206874
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-02-15 14:58:18 +0530

    lecture 146 A: API versioning : Request Param versioning

--- restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -5,15 +5,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
-	
+
 	@GetMapping("v1/person")
 	public PersonV1 getFirstVersionPerson() {
 		return new PersonV1("Bob Charlie");
 	}
-	
+
 	@GetMapping("v2/person")
 	public PersonV2 getSecondVersionPerson() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+
+	@GetMapping(path = "/person", params = "ver=1")
+	public PersonV1 getPerson1BasedOnRequestParameter() {
+		return new PersonV1("Alice Singh");
+	}
+	
+	@GetMapping(path = "/person", params = "ver=2")
+	public PersonV2 getPerson2BasedOnRequestParameter() {
+		return new PersonV2(new Name("Bob", "Kumar"));
+	}
+
 
 }
