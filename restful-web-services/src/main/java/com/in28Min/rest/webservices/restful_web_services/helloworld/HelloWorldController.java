edbaa2fad83c2b4ca17c85266cd70a6cff55ecfa
Rishu Toshniwal <rishutoshniwal73@gmail.com>
2026-02-15 14:15:53 +0530

    lecture 143: Internationalization- accepting language header

--- restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28Min/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.in28Min.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "hello-world")
 	public String helloWorld() {
@@ -20,6 +30,12 @@ public class HelloWorldController {
 	@GetMapping(path = "hello-world-bean/{name}")
 	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
 		return new HelloWorldBean("Hello-World-Bean" + " " + name);
+	}
+
+	@GetMapping(path = "good-morning-internationalized")
+	public String goodMorningInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "default message", locale);
 	}
 
 }
